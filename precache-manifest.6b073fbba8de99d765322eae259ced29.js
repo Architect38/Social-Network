af87687f0867616acc4d80ffc5c743e7220328e8
Architect38 <gothic38101@gmail.com>
2019-12-06 17:50:59 +0400

    Updates

---
+++ precache-manifest.6b073fbba8de99d765322eae259ced29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abe183ceec82ee9f8a5765336709574",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "3cdbb2cf43f78e866c3c",
+    "url": "/Social-Network/static/css/main.d573eb1b.chunk.css"
+  },
+  {
+    "revision": "cce25aa66ebb4340f4fa",
+    "url": "/Social-Network/static/js/2.5b409d2e.chunk.js"
+  },
+  {
+    "revision": "3cdbb2cf43f78e866c3c",
+    "url": "/Social-Network/static/js/main.d3354391.chunk.js"
+  },
+  {
+    "revision": "ae20d3d73e4f62acf52a",
+    "url": "/Social-Network/static/js/runtime-main.f0ce605c.js"
+  }
+]);
