dd1e3a147cbffbd16c5492b13a562c9c2ef142ee
Architect38 <gothic38101@gmail.com>
2019-12-10 14:06:20 +0400

    Updates

--- precache-manifest.7a6692c4bec5952b5398de1755b23654.js
+++ precache-manifest.7a6692c4bec5952b5398de1755b23654.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82762fd34a0ec295207f3e8032dd5491",
+    "revision": "56c32e9eb46e20c139ec784f8541ad57",
     "url": "/Social-Network/index.html"
   },
   {
-    "revision": "353fe52ccd0839521f81",
+    "revision": "88102feea290b878fa8d",
     "url": "/Social-Network/static/css/main.d91d21b5.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Social-Network/static/js/2.50a1fbb7.chunk.js"
   },
   {
-    "revision": "353fe52ccd0839521f81",
-    "url": "/Social-Network/static/js/main.40468312.chunk.js"
+    "revision": "88102feea290b878fa8d",
+    "url": "/Social-Network/static/js/main.e1ab8135.chunk.js"
   },
   {
     "revision": "ae20d3d73e4f62acf52a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Social-Network/precache-manifest.2d181fab6135e2cca65fdb9189f656bb.js"
+  "/Social-Network/precache-manifest.7a6692c4bec5952b5398de1755b23654.js"
 );
 
 self.addEventListener('message', (event) => {
